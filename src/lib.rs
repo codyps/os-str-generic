ac57ec710422eeb44136992bda6228f5398450c7
Cody P Schafer <dev@codyps.com>
2017-02-13 19:07:59 -0500

    minimal api, generic iterator over generic elements only

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use std::ffi::{OsString, OsStr};
+#[deny(unused_imports)]
+
+use std::ffi::OsStr;
 
 #[cfg(windows)]
 type Elem = u16;
@@ -6,9 +8,15 @@ type Elem = u16;
 #[cfg(not(windows))]
 type Elem = u8;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct OsStrElement {
     inner: Elem 
+}
+
+impl<'a> ::std::cmp::PartialEq<&'a OsStr> for OsStrElement {
+    fn eq(&self, rhs: & &'a OsStr) -> bool { 
+        rhs.len() == 1 && rhs.elements().next().unwrap().inner == self.inner
+    }
 }
 
 #[cfg(windows)]
@@ -24,7 +32,7 @@ impl<'a> ::std::fmt::Debug for OsStrElementsInner<'a> {
 
 #[cfg(not(windows))]
 #[derive(Debug, Clone)]
-struct OsStrElementsInner<'a>(std::slice::Iter<'a, u8>);
+struct OsStrElementsInner<'a>(std::iter::Cloned<std::slice::Iter<'a, u8>>);
 
 impl<'a> OsStrElementsInner<'a> {
     #[cfg(windows)]
@@ -36,7 +44,7 @@ impl<'a> OsStrElementsInner<'a> {
     #[cfg(not(windows))]
     fn from_osstr(i: &'a OsStr) -> OsStrElementsInner<'a> {
         use std::os::unix::ffi::OsStrExt;
-        OsStrElementsInner(i.as_bytes().iter())
+        OsStrElementsInner(i.as_bytes().iter().cloned())
     }
 }
 
@@ -49,15 +57,24 @@ impl<'a> Iterator for OsStrElements<'a> {
     type Item = OsStrElement;
 
     fn next(&mut self) -> Option<OsStrElement> {
-        None     
+        self.inner.0.next().map(|x| OsStrElement { inner: x })
     }
 }
 
 trait OsStrGenericExt {
+    // type Element;
     fn elements(&self) -> OsStrElements;
 }
 
 impl OsStrGenericExt for OsStr {
+    /*
+    /* consider this pattern, perhaps in another trait */
+    #[cfg(windows)]
+    type Element = u16;
+    #[cfg(not(windows))]
+    type Element = u8;
+    */
+
     fn elements(&self) -> OsStrElements {
         OsStrElements {
             inner: OsStrElementsInner::from_osstr(self),
@@ -87,7 +104,14 @@ fn split_flags(flags: &OsStr) -> OsStrSplit
 
 #[cfg(test)]
 mod tests {
+    use super::OsStrGenericExt;
+    use std::ffi::{OsStr};
+
     #[test]
-    fn it_works() {
+    fn elements_eq() {
+        let mut v = OsStr::new("hi").elements();
+        assert_eq!(v.next().unwrap(), OsStr::new("h"));
+        assert_eq!(v.next().unwrap(), OsStr::new("i"));
+        assert_eq!(v.next(), None);
     }
 }
